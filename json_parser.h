d13be8a9a99ece92582794555364211f9ec15d2a
Roman Borisov <rborisov@blackberry.com>
2015-10-30 17:29:02 +0300

    First commit

---
+++ json_parser.h
@@ -0,0 +1,8 @@
+#ifndef __JSON_PARSE_H__
+#define __JSON_PARSE_H__
+
+#include <json-c/json.h>
+
+void json_parse(json_object *);
+
+#endif //__JSON_PARSE_H__
---
+++ main.c
@@ -0,0 +1,85 @@
+/* <DESC>
+ * simple HTTP POST using the easy interface
+ * </DESC>
+ */ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+
+#include "json_parser.h"
+
+struct MemoryStruct {
+    char *memory;
+    size_t size;
+};
+
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    size_t realsize = size * nmemb;
+    struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+    mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+    if(mem->memory == NULL) {
+        /* out of memory! */ 
+        printf("not enough memory (realloc returned NULL)\n");
+        return 0;
+    }
+             
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+
+    return realsize;
+}
+
+int main(void)
+{
+    CURL *curl;
+    CURLcode res;
+    char search_str[128] = "";
+    struct MemoryStruct chunk;
+
+char * string = "{\"sitename\" : \"joys of programming\", \"categories\" : [ \"c\" , [\"c++\" , \"c\" ], \"java\", \"PHP\" ], \"author-details\": { \"admin\": false, \"name\" : \"Joys of Programming\", \"Number of Posts\" : 10 } }";
+
+    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
+    chunk.size = 0;    /* no data at this point */ 
+  
+    sprintf(search_str, "http://api.dirble.com/v2/search/%s?token=acd53b8af2e49415ce696bac4e", "paradise");
+ 
+    /* In windows, this will init the winsock stuff */ 
+    curl_global_init(CURL_GLOBAL_ALL);
+ 
+    /* get a curl handle */ 
+    curl = curl_easy_init();
+    if(curl) {
+        /* First set the URL that is about to receive our POST. This URL can
+           just as well be a https:// URL if that is what should receive the
+           data. */ 
+        curl_easy_setopt(curl, CURLOPT_URL, search_str);
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+
+        /* we pass our 'chunk' struct to the callback function */ 
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+
+        /* Perform the request, res will get the return code */ 
+        res = curl_easy_perform(curl);
+        /* Check for errors */ 
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+ 
+        /* always cleanup */ 
+        curl_easy_cleanup(curl);
+    }
+
+    //printf("%s\n", chunk.memory);
+    
+    json_object * jobj = json_tokener_parse(string);
+    json_parse(jobj);
+    
+    free(chunk.memory);
+    curl_global_cleanup();
+    return 0;
+}
