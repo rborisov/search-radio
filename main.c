80e8e98a8c521960f810bda3ddcbd4a5c598ef7e
Roman Borisov <roman.borisov@harman.com>
2016-09-22 18:45:15 +0300

    json to struct - initial commit

--- main.c
+++ main.c
@@ -9,6 +9,22 @@
 
 #include "json.h"
 
+typedef struct {
+    int id;
+    char name[128];
+    char description[128];
+    char image[128];
+    char stream[128];
+} DIRSTREAM;
+
+typedef struct {
+    size_t size;
+    int num;
+    DIRSTREAM **streams;
+} DIRSTREAMS;
+
+DIRSTREAMS dirstreams;
+
 struct MemoryStruct {
     char *memory;
     size_t size;
@@ -37,23 +53,54 @@ static void print_depth_shift(int depth)
 {
     int j;
     for (j=0; j < depth; j++) {
-        printf(" ");
+        printf("-");
     }
 }
 
 static void process_value(json_value* value, int depth);
+int process_int(json_value* value, int depth);
+
+int append_stream()
+{
+    dirstreams.streams = realloc(dirstreams.streams, sizeof(DIRSTREAM*)*(dirstreams.num+1));
+    if (dirstreams.streams == NULL)
+        return -1;
+    dirstreams.streams[dirstreams.num] = malloc(sizeof(DIRSTREAM));
+    if (dirstreams.streams[dirstreams.num] == NULL)
+        return -2;
+    return dirstreams.num++;
+}
+
+void free_streams()
+{
+    while (dirstreams.num--) {
+        printf("%d %d\n", dirstreams.num, dirstreams.streams[dirstreams.num]->id);
+        free(dirstreams.streams[dirstreams.num]);
+    }
+    free(dirstreams.streams);
+}
 
 static void process_object(json_value* value, int depth)
 {
-    int length, x;
+    int length, x, i;
     if (value == NULL) {
         return;
     }
     length = value->u.object.length;
+    printf("{\n");
     for (x = 0; x < length; x++) {
         print_depth_shift(depth);
-        printf("object[%d].name = %s\n", x, value->u.object.values[x].name);
-        process_value(value->u.object.values[x].value, depth+1);
+        printf("(%d).name = %s\n", x, value->u.object.values[x].name);
+        if (strcmp(value->u.object.values[x].name, "id") == 0 && depth == 2) {
+            //alloc new dirstr and get id
+            i = append_stream();
+            if (i < 0) {
+                return;
+            }
+            dirstreams.streams[i]->id = process_int(value->u.object.values[x].value, depth+1);
+        } else {
+            process_value(value->u.object.values[x].value, depth+1);
+        }
     }
 }
 
@@ -64,11 +111,29 @@ static void process_array(json_value* value, int depth)
         return;
     }
     length = value->u.array.length;
-    printf("array\n");
+    printf("[\n");
     for (x = 0; x < length; x++) {
         process_value(value->u.array.values[x], depth);
     }
 }
+
+int process_int(json_value* value, int depth)
+{
+    int j;
+    if (value == NULL) {
+        printf("%s: value == NULL\n", __func__);
+        return 0;
+    }
+    if (value->type == json_integer) {
+        printf("int: %d\n", value->u.integer);
+        return value->u.integer;
+    } else {
+        printf("%s: value->type != json_integer\n", __func__);
+        process_value(value, depth);
+    }
+    return 0;
+}
+
 
 static void process_value(json_value* value, int depth)
 {
@@ -76,6 +141,8 @@ static void process_value(json_value* value, int depth)
     if (value == NULL) {
         return;
     }
+    if (depth > 6)
+        return;
     if (value->type != json_object) {
         print_depth_shift(depth);
     }
@@ -112,15 +179,15 @@ int main(void)
     struct MemoryStruct chunk;
     json_char* json;
     json_value* value;
-
-char * string = "{\"sitename\" : \"joys of programming\", \"categories\" : [ \"c\" , [\"c++\" , \"c\" ], \"java\", \"PHP\" ], \"author-details\": { \"admin\": false, \"name\" : \"Joys of Programming\", \"Number of Posts\" : 10 } }";
-
     chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
-    chunk.size = 0;    /* no data at this point */ 
+    chunk.size = 0;    /* no data at this point */
+
+   dirstreams.num = 0;
+   dirstreams.size = 0;
+//   dirstreams.streams = (void*)malloc(sizeof(dirstream_ *));
   
-    sprintf(search_str, "http://api.dirble.com/v2/search/%s?token=acd53b8af2e49415ce696bac4e", "paradise");
+    sprintf(search_str, "http://api.dirble.com/v2/search/%s?token=acd53b8af2e49415ce696bac4e", "Paradise");
  
-    /* In windows, this will init the winsock stuff */ 
     curl_global_init(CURL_GLOBAL_ALL);
  
     /* get a curl handle */ 
@@ -130,28 +197,20 @@ char * string = "{\"sitename\" : \"joys of programming\", \"categories\" : [ \"c
            just as well be a https:// URL if that is what should receive the
            data. */ 
         curl_easy_setopt(curl, CURLOPT_URL, search_str);
-
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-
         /* we pass our 'chunk' struct to the callback function */ 
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
-
         /* Perform the request, res will get the return code */ 
         res = curl_easy_perform(curl);
         /* Check for errors */ 
         if(res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
                     curl_easy_strerror(res));
- 
         /* always cleanup */ 
         curl_easy_cleanup(curl);
     }
 
     //printf("%s\n", chunk.memory);
-    
-    //json_object * jobj = json_tokener_parse(chunk.memory);
-    //json_parse(jobj);
-   
     json = (json_char*)chunk.memory;
     value = json_parse(json, chunk.size);
     if (value == NULL) {
@@ -161,8 +220,10 @@ char * string = "{\"sitename\" : \"joys of programming\", \"categories\" : [ \"c
         return 0;
     }
     process_value(value, 0);
-    json_value_free(value);
 
+    free_streams();
+
+    json_value_free(value);
     free(chunk.memory);
     curl_global_cleanup();
     return 0;
